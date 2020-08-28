b52312d2f0ea049990c0b3a4baf8f3149e2f9bd3
Ashish Kathuria <xs2ashish@gmail.com>
2020-08-28 00:17:44 +0000

    multiple updates

--- 01-example/run_playbook-01.sh
+++ 01-example/run_playbook-01.sh
@@ -19,17 +19,12 @@
 CURR_DIR=`dirname $0`
 server_list=$1
 inventory_file=$2
-
-echo $server_list
-echo $inventory_file
 #check if all is passed
 is_all=`echo "$server_list"|tr '[A-Z]' '[a-z]'`
-echo $is_all
 if [ "$is_all" = "all" ]; then
 echo "ALL parameter is not allowed. Exiting script. Please provide comma separated list of server names."
 exit
 else
-export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3
 export ANSIBLE_STDOUT_CALLBACK=debug
-ansible-playbook --extra-vars "server_list=${server_list}" -i ${inventory_file} -u ssh_user -vv ${CURR_DIR}/playbooks/playbook-01.yml
+ansible-playbook --extra-vars "server_list=${server_list}" -i ${inventory_file} -u ssh_user -v ${CURR_DIR}/playbooks/playbook-01.yml
 fi
